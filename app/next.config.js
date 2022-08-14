3caa2101f603d6799bb10f57fb279726a17cd103
MrToph <MrToph@users.noreply.github.com>
2022-08-14 09:23:40 +0000

    set base path for gh pages

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/nft-enhancement', // make it work with github pages
 };
 
 module.exports = nextConfig;
